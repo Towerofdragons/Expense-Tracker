45672f0d1aba632d039540b0c657e45308256046
Rodney Njau <marknjoropge@gmail.com>
2025-03-09 23:12:06 +0300

    Separate Home and Dashboard Controllers

--- Backend/Controllers/HomeController.cs
+++ Backend/Controllers/HomeController.cs
@@ -2,6 +2,8 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Backend.Models;
 
+using Microsoft.AspNetCore.Authorization;
+
 namespace Backend.Controllers;
 
 public class HomeController : Controller
@@ -30,49 +32,7 @@ public class HomeController : Controller
     }
 }
 
-public class DashboardController : Controller
-{
 
-    private readonly TrackerDBContext _context;
-
-    public  DashboardController (TrackerDBContext context)
-    {
-      _context =  context;
-    }
-    public IActionResult Index()
-    {
-        var Expenses = _context.Expense.ToList();
-        var Incomes = _context.Income.ToList();
-
-        decimal TotalIncomes = 0;
-        decimal TotalExpenses = 0;
-
-        foreach (var expense in Expenses)
-        {
-            TotalExpenses += (decimal)expense.Amount;
-        }
-
-        foreach (var income in Incomes)
-        {
-            TotalIncomes += (decimal)income.Amount;
-        }
-
-        
-        var model = new IncomeExpenses
-        {
-            TotalExpense = TotalExpenses,
-            TotalIncome = TotalIncomes,
-            Expense = _context.Expense.ToList(),
-            Income = _context.Income.ToList()
-        };
-
-        return View(model);
-    }
-
-   
-
-    
-}
 
     public class IncomeController : Controller
     {
