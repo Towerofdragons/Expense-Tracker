08b3f8a7c3ddc29d4c790a15ee25d4e19e53c518
Rodney Njau <marknjoropge@gmail.com>
2025-03-09 18:06:14 +0300

    Add CRUD operations for Income and Expenses

--- Backend/Controllers/HomeController.cs
+++ Backend/Controllers/HomeController.cs
@@ -68,4 +68,171 @@ public class DashboardController : Controller
 
         return View(model);
     }
+
+   
+
+    
 }
+
+    public class IncomeController : Controller
+    {
+        private readonly TrackerDBContext _context;
+
+        public IncomeController(TrackerDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(Income income)
+        {
+            if (income != null)
+            {
+                _context.Income.Add(income);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+    
+        // Edit Income - GET
+        public IActionResult Edit(int id)
+        {
+            var income = _context.Income.Find(id);
+            if (income == null)
+            {
+                return NotFound();
+            }
+            return View(income);
+        }
+
+        // Edit Income - POST
+        [HttpPost]
+        public IActionResult Edit(int id, Income updatedIncome)
+        {
+            var income = _context.Income.Find(id);
+            if (income == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                income.Amount = updatedIncome.Amount;
+                income.Description = updatedIncome.Description;
+                income.IncomeDate = updatedIncome.IncomeDate;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(updatedIncome);
+        }
+
+        // Delete Income - GET
+        // public IActionResult Delete(int id)
+        // {
+        //     var income = _context.Income.Find(id);
+        //     if (income == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     return View(income);
+        // }
+
+        // Delete Income - POST
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var income = _context.Income.Find(id);
+            if (income == null)
+            {
+                return NotFound();
+            }
+
+            _context.Income.Remove(income);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+
+
+
+
+    public class ExpenseController : Controller
+    {
+        private readonly TrackerDBContext _context;
+
+        public ExpenseController(TrackerDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(Expense expense)
+        {
+            if (expense != null)
+            {
+                _context.Expense.Add(expense);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // Edit Expense - GET
+        public IActionResult Edit(int id)
+        {
+            var expense = _context.Expense.Find(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            return View(expense);
+        }
+
+        // Edit Expense - POST
+        [HttpPost]
+        public IActionResult Edit(int id, Expense updatedExpense)
+        {
+            var expense = _context.Expense.Find(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                expense.Amount = updatedExpense.Amount;
+                expense.Description = updatedExpense.Description;
+                expense.ExpenseDate = updatedExpense.ExpenseDate;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(updatedExpense);
+        }
+
+        // Delete Expense - GET
+        // public IActionResult Delete(int id)
+        // {
+        //     var expense = _context.Expense.Find(id);
+        //     if (expense == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     return View(expense);
+        // }
+
+        // Delete Expense - POST
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var expense = _context.Expense.Find(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            _context.Expense.Remove(expense);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+
