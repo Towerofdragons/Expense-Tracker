88f8448e5b5e7d1f927d337aa29ef627cb19e848
Rodney Njau <marknjoropge@gmail.com>
2025-03-10 08:05:23 +0300

    Fix Income And outcome data tables and forms rendering order

--- Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a79068a1ff154ac64ffe5f26cc59460abf7acb9b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d1251927fd270a7554b8c354fcd84856c1b4e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
